34f6b09001d9cccd05a45f94be7e773aaa55d100
nicejade <jeffygisgreat@gmail.com>
2018-07-06 20:11:54 +0800

    ✨ optimize webpack packaging

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
+/** @format */
+
 const path = require('path')
 const fs = require('fs')
 const PrerenderSPAPlugin = require('prerender-spa-plugin')
@@ -75,6 +77,41 @@ module.exports = {
         name: '[name]-[hash:7]',
         prefixize: true
       })
+
+    const splitOptions = config.optimization.get('splitChunks')
+    config.optimization.splitChunks(
+      Object.assign({}, splitOptions, {
+        // （缺省值5）按需加载时的最大并行请求数
+        maxAsyncRequests: 16,
+        // （默认值3）入口点上的最大并行请求数
+        maxInitialRequests: 16,
+        // （默认值：1）分割前共享模块的最小块数
+        minChunks: 1,
+        // （默认值：30000）块的最小大小
+        minSize: 30000,
+        // webpack 将使用块的起源和名称来生成名称: `vendors~main.js`,如项目与"~"冲突，则可通过此值修改，Eg: '-'
+        automaticNameDelimiter: '~',
+        // cacheGroups is an object where keys are the cache group names.
+        name: true,
+        cacheGroups: {
+          default: false,
+          common: {
+            name: `chunk-common`,
+            minChunks: 2,
+            priority: -20,
+            chunks: 'initial',
+            reuseExistingChunk: true
+          },
+          element: {
+            name: 'element',
+            test: /[\\/]node_modules[\\/]element-ui[\\/]/,
+            chunks: 'initial',
+            // 默认组的优先级为负数，以允许任何自定义缓存组具有更高的优先级（默认值为0）
+            priority: -30
+          }
+        }
+      })
+    )
 
     // https://github.com/webpack-contrib/webpack-bundle-analyzer
     if (process.env.npm_config_report) {
