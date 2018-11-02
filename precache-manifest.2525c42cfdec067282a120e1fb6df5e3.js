a5f4662ea7735db71368abd2c86e00f0845165e4
JadeYang(杨琼璞) <jeffygisgreat@gmail.com>
2018-11-02 23:59:26 +0800

    🎉 update release

---
+++ precache-manifest.2525c42cfdec067282a120e1fb6df5e3.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "96fb4309d0f85b02c624fac6f16dd0fc",
+    "url": "/img/vue-cli-npm.96fb4309.png"
+  },
+  {
+    "revision": "bf35c3805e83f0178f9e224d8d0febca",
+    "url": "/service-worker.js"
+  },
+  {
+    "revision": "2fad952a20fbbcfd1bf2ebb210dccf7a",
+    "url": "/fonts/element-icons.2fad952a.woff"
+  },
+  {
+    "revision": "5acea3d4f56a03e49dcc",
+    "url": "/css/chunk-4552f9ae.d15fab60.css"
+  },
+  {
+    "revision": "b1dbbe1542966855552c",
+    "url": "/css/chunk-aea9e572.6ee35604.css"
+  },
+  {
+    "revision": "b1dbbe1542966855552c",
+    "url": "/js/chunk-aea9e572.d8c93f85.js"
+  },
+  {
+    "revision": "0834488477a889b8b0fa",
+    "url": "/js/element.992944a9.js"
+  },
+  {
+    "revision": "f6d9e3a097009628e4344228dcae4288",
+    "url": "/img/vue-cli-manage-project-dependencies.f6d9e3a0.png"
+  },
+  {
+    "revision": "2aad7f36c2e53db18db9",
+    "url": "/js/app.1f41f7fe.js"
+  },
+  {
+    "revision": "5acea3d4f56a03e49dcc",
+    "url": "/js/chunk-4552f9ae.35e9747b.js"
+  },
+  {
+    "revision": "6f0a76321d30f3c8120915e57f7bd77e",
+    "url": "/fonts/element-icons.6f0a7632.ttf"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "bece2ac24db0b9ce53b43f58989e3af3",
+    "url": "/img/vue-cli-manage-cli-plugin.bece2ac2.png"
+  },
+  {
+    "revision": "0fe3f69d2eb7c5255a8b838cb72da2b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "2aad7f36c2e53db18db9",
+    "url": "/css/app.fba487c0.css"
+  }
+];
